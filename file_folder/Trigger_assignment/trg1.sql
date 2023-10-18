02c1be326640315503ef3c0e9f629444cfc3286a
74489ritesh <riteshmalik@gmail.com>
2023-10-18 15:44:52 -0700

    fc

--- file_folder/Trigger_assignment/trg1.sql
+++
@@ -1,7 +0,0 @@
-drop TRIGGER if exists insertStudent;
-delimiter $
-create TRIGGER insertStudent before INSERT on insert_students for each ROW
-BEGIN
-	insert into log VALUES(NEW.id,CURRENT_DATE,CURRENT_TIME,'Record inserted successfully');
-end $
-delimiter ;
